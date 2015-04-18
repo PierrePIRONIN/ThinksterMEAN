3c714c7eb75585518069267887e18159a445f069
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-04-18 11:29:31 +0200

    Add post with form. Enable up vote.

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainController', function() {
-   this.test = "Hello world !"
+   this.posts = [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+   ];
+
+   this.currentPost = {}
+
+   this.addPost = function() {
+      if (!this.currentPost.title || this.currentPost.title == '') {
+         return;
+      }
+      this.currentPost.upvotes = 0;
+      this.posts.push(this.currentPost);
+      this.currentPost = {}
+   }
+
+   this.incrementUpvote = function(post) {
+      post.upvotes += 1;
+   }
 });
