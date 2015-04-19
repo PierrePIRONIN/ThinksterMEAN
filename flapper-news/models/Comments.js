48712837f0f65230501d3f77a0814ad255754129
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-04-19 21:30:42 +0200

    Add Mongoose models.

---
+++ flapper-news/models/Comments.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var CommentSchema = mongoose.Schema({
+    author: String,
+    body: String,
+    upvotes: {type: Number, default: 0},
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+});
+
+mongoose.model('Comment', CommentSchema);
---
+++ flapper-news/models/Posts.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var PostSchema = mongoose.Schema({
+    title: String,
+    link: String,
+    upvotes: {type: Number, default: 0},
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+});
+
+mongoose.schema('Post', PostSchema);
