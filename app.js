19cae943eeeccb0b5f599c489ebff045b3d15c27
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-04-19 15:51:25 +0200

    Add a route for /posts/{id}

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
             url: '/home',
             templateUrl: '/home.html',
             controller: 'MainController as mainCtrl'
+        })
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/post.html',
+            controller: 'PostsController as postsCtrl'
         });
 
     $urlRouterProvider.otherwise('home');
@@ -15,18 +20,57 @@ app.factory('posts', function () {
     var service = {};
 
     service.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 4}
+        {
+            title: 'post 1',
+            upvotes: 5,
+            comments: [
+                {author: 'me', body: 'Fake 1', upvotes: 0},
+                {author: 'you', body: 'Fake 2', upvotes: 0},
+                {author: 'him', body: 'Fake 3', upvotes: 0},
+            ]
+        },
+        {
+            title: 'post 2',
+            upvotes: 2,
+            comments: [
+                {author: 'me', body: 'Fake 11', upvotes: 0},
+                {author: 'you', body: 'Fake 22', upvotes: 0},
+                {author: 'him', body: 'Fake 33', upvotes: 0},
+            ]
+        },
+        {
+            title: 'post 3',
+            upvotes: 15,
+            comments: [
+                {author: 'me', body: 'Fake 111', upvotes: 0},
+                {author: 'you', body: 'Fake 222', upvotes: 0},
+                {author: 'him', body: 'Fake 333', upvotes: 0},
+            ]
+        },
+        {
+            title: 'post 4',
+            upvotes: 9,
+            comments: [
+                {author: 'me', body: 'Fake 1111', upvotes: 0},
+                {author: 'you', body: 'Fake 2222', upvotes: 0},
+                {author: 'him', body: 'Fake 3333', upvotes: 0},
+            ]
+        },
+        {
+            title: 'post 5',
+            upvotes: 4,
+            comments: [
+                {author: 'me', body: 'Fake 11111', upvotes: 0},
+                {author: 'you', body: 'Fake 22222', upvotes: 0},
+                {author: 'him', body: 'Fake 33333', upvotes: 0},
+            ]
+        }
     ];
 
     return service;
 });
 
 app.controller('MainController', ['posts', function (posts) {
-
     this.currentPost = {}
 
     this.getPosts = function () {
@@ -47,4 +91,12 @@ app.controller('MainController', ['posts', function (posts) {
     this.incrementUpvote = function (post) {
         post.upvotes += 1;
     }
+}]);
+
+app.controller('PostsController', ['$stateParams', 'posts', function ($stateParams, posts) {
+    this.post = posts.posts[$stateParams.id];
+
+    //this.addComment = function() {
+    //
+    //}
 }]);
