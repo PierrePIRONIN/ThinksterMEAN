ed0966a8e09bce90384490421e5703c5195157b9
Pierre PIRONIN <pierre@pierrepironin.fr>
2015-04-19 15:06:34 +0200

    Add a route for /home.

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
-app.factory('posts', function() {
-   var service = {};
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainController as mainCtrl'
+        });
 
-   service.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-   ];
+    $urlRouterProvider.otherwise('home');
+}]);
 
-   return service;
+app.factory('posts', function () {
+    var service = {};
+
+    service.posts = [
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 9},
+        {title: 'post 5', upvotes: 4}
+    ];
+
+    return service;
 });
 
-app.controller('MainController', ['posts', function(posts) {
+app.controller('MainController', ['posts', function (posts) {
 
-   this.currentPost = {}
+    this.currentPost = {}
 
-   this.getPosts = function() {
-     return posts.posts;
-   };
+    this.getPosts = function () {
+        return posts.posts;
+    };
 
-   this.addPost = function() {
-      if (!this.currentPost.title || this.currentPost.title == '') {
-         return;
-      }
+    this.addPost = function () {
+        if (!this.currentPost.title || this.currentPost.title == '') {
+            return;
+        }
 
-      this.currentPost.upvotes = 0;
-      posts.posts.push(this.currentPost);
+        this.currentPost.upvotes = 0;
+        posts.posts.push(this.currentPost);
 
-      this.currentPost = {}
-   }
+        this.currentPost = {}
+    }
 
-   this.incrementUpvote = function(post) {
-      post.upvotes += 1;
-   }
+    this.incrementUpvote = function (post) {
+        post.upvotes += 1;
+    }
 }]);
